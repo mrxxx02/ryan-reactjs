22c2c5933b08d4e7221634988b636ba81ac22ddc
Ryanprayyy <79523005+mrxxx02@users.noreply.github.com>
2022-07-17 13:17:30 +0700

    Update ClassComponent.jsx

--- src/Materi/Komponen/pembahasan/ClassComponent.jsx
+++ src/Materi/Komponen/pembahasan/ClassComponent.jsx
@@ -1,37 +1,37 @@
-import React  from "react";
+import React from "react";
 
 class ClassComponent extends React.Component {
     constructor(props) {
         super(props)
-        
+
         this.state = {
             value: 0
         }
+        this.handleMinus = this.handleMinus.bind(this)
+        this.handlePlus = this.handlePlus.bind(this)
 
-        this.handlePlus = this.handlePlus.bind(this);
-        this.handleMinus = this.handleMinus.bind(this);
     }
 
-    handlePlus() {
-        this.setState({value: this.state.value +1});
+    handlePlus(){
+        this.setState({value: this.state.value + 1 });
     }
-    handleMinus() {
-        if(this.state.value > 0) {
-        this.setState({value: this.state.value -1});
+    handleMinus(){
+        if(this.state.value >0) {
+        this.setState({value: this.state.value - 1 });
         }
     }
 
     render() {
         return(
             <div>
-                <h1>Komponen ini dibuat menggunakan ClassComponent</h1>
-                <h2>Hallo {this.props.nama}</h2>
-                <button>-</button>
+                <h1>Komponen ini dibuat dengan class component</h1>
+                <h2>Hello {this.props.nama}</h2>
+                <button onClick={this.handleMinus}>-</button>
                 <span>{' '} {this.state.value} {' '}</span>
-                <button onClick={ this.handlePlus}>+</button>
+                <button onClick={this.handlePlus}>+</button>
             </div>
         )
     }
 }
 
-export default ClassComponent;
+export default ClassComponent
