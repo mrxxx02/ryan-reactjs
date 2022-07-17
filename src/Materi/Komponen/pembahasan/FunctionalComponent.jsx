1b90a87d3fb21d7826f64fb640565e862f2c32f5
Ryanprayyy <79523005+mrxxx02@users.noreply.github.com>
2022-07-17 13:17:00 +0700

    Update FunctionalComponent.jsx

--- src/Materi/Komponen/pembahasan/FunctionalComponent.jsx
+++ src/Materi/Komponen/pembahasan/FunctionalComponent.jsx
@@ -1,30 +1,25 @@
 import { useState } from "react";
 
-const functionalComponent= ({nama}) => {
+const FunctionalComponent = ({nama}) => {
     const [value, setValue] = useState(0);
-
+   
     const handlePlus = () => {
-        setValue(value + 1 );
+        setValue(value + 1);
     }
-
     const handleMinus = () => {
-        if (value >0) {
-        setValue(value - 1 );
+        if(value >0) {
+        setValue(value - 1);
         }
     }
-
     return (
         <div>
             <h1>Membuat Komponen dengan Functional Component</h1>
-            <h2>Hallo {nama}, Selamat Belajar</h2>
+            <h2>Halo{nama}, Selamat Belajar!</h2>
             <button onClick={handleMinus}>-</button>
-            <span>{' '} {value} {' '}</span>
+            <span>{' '}{value} {' '} </span>
             <button onClick={handlePlus}>+</button>
         </div>
     )
 }
 
-functionalComponent.defaultProps = {
-    nama: 'User'
-}
-export default functionalComponent;
+export default FunctionalComponent;
